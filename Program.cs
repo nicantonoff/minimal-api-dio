4c41652743e902a64edffb4ba08aa0e93fd25c47
nicantonoff <nicolas.antonoff@gmail.com>
2024-09-24 11:16:42 -0300

    Cria Interfaces e Servicos para Administrador e Veiculo

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options => {
     options.UseMySql(
@@ -13,10 +18,10 @@ builder.Services.AddDbContext<DbContexto>(options => {
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Olá pessoal!");
 
-app.MapPost("/login", (LoginDTO loginDTO) => {
-    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico ) => {
+    if(administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login efetuado com sucesso");
     else
         return Results.Unauthorized();
